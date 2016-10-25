9645e65615d05e571d8aa553d07451923fd3201e
Joao Roberto Lima <jbetol@gmail.com>
2016-10-25 17:48:12 -0200

    styles, scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
+size = 0;
 $(document).ready(function(){
-	$('.wrapper').on('click', function(){
-		alert("hovering");
+	$('#button').on('click', function(){
+		size = prompt("Informe tamanho dos pixels");
+		alert(size);
+		fillYellowed();
 	})
 })
+
+function fillYellowed(){
+/*		alert("INSIDE FILL "+size);
+}	
+*/	for(var i = 0; i < size; i++){
+		for (var j = 0; j < size; j++){
+			$(".wrapper".append($(".pixels"));
+		}
+	}
+//	alert("Saindo FILL "+ i);
+	
+}
+
