1722c9cedc8e09f7f2d95dfc055b1d9ccce2e971
Joao Roberto Lima <jbetol@gmail.com>
2016-10-26 17:54:40 -0200

    new lines

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,24 @@ size = 0;
 $(document).ready(function(){
 	$('#button').on('click', function(){
 		size = prompt("Informe tamanho dos pixels");
-		alert(size);
+//		alert(size);
 		fillYellowed();
+	$('.wrapper').on()	
 	})
 })
 
 function fillYellowed(){
-/*		alert("INSIDE FILL "+size);
-}	
-*/	for(var i = 0; i < size; i++){
+//		alert("INSIDE FILL "+size);
+//}	
+	for(var i = 0; i < size; i++){
 		for (var j = 0; j < size; j++){
-//			$(".wrapper".append($(".pixels"));
+			$(".wrapper").append($('.pixels'));
 		}
 	}
 	alert("Saindo FILL "+ i);
 	
 }
-
+/*
+$( "p" ).add( "div" ).addClass( "widget" );
+var pdiv = $( "p" ).add( "div" );
+*/
